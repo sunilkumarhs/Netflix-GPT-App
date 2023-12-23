b6d33142cc8d90630c9d96bd7c3c2bb30ad45a05
sunilkumarhs <sunilkumarhs974117@gmail.com>
2023-12-23 22:42:06 +0530

    Login page complete

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,14 +1,102 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <div className="overflow-hidden relative">
+    <div className="m-0 min-h-full p-0 relative z-0">
+      <div className="overflow-hidden absolute min-h-[100vh] w-full -z-10 block">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/6b2842d1-2339-4f08-84f6-148e9fcbe01b/IN-en-20231218-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="bg-img"
-          className="scale-125 w-full h-full"
+          className="h-[1080px] min-w-full "
         />
+        <div className="absolute top-0 right-0 bottom-0 left-0 signBg1">
+          <div className="absolute top-0 right-0 bottom-0 left-0 signBg2">
+            <div className="w-full flex">
+              <div className="px-4">
+                <img
+                  className="w-56"
+                  src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+                  alt="logo"
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <div className="signInput mx-[405px] p-[4rem]">
+                <h1 className="text-4xl font-semibold text-white text-left">
+                  Sign In
+                </h1>
+                <div className="text-left py-6">
+                  <input
+                    placeholder="Email or phone number"
+                    className="text-l text-white my-2 w-full py-3 px-6 bg-zinc-700 rounded-sm"
+                  />
+                  <input
+                    placeholder="password"
+                    className="text-l text-white my-2 w-full py-3 px-6 bg-zinc-700 rounded-sm"
+                  />
+                  <button className="text-l w-full mt-6 font-semibold text-white bg-red-600 rounded-sm px-14 py-3">
+                    Sign In
+                  </button>
+                  <div className="flex justify-between my-2">
+                    <p className="text-gray-400 text-sm cursor-pointer">
+                      Remember me
+                    </p>
+                    <p className="text-gray-400 text-sm cursor-pointer">
+                      Need help?
+                    </p>
+                  </div>
+                  <div className="py-12">
+                    <p className="text-l text-slate-500">
+                      New to Netflix?{" "}
+                      <span
+                        className="text-l text-white font-semibold cursor-pointer"
+                        onClick={() => {
+                          navigate("/");
+                        }}
+                      >
+                        Sign up now
+                      </span>
+                    </p>
+                    <p className="text-sm text-slate-400 py-3">
+                      Sign in is protected by Google reCAPTCHA to ensure You're
+                      not a bot.{" "}
+                      <span className="text-blue-600 cursor-pointer">
+                        Learn more
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="pt-20">
+              <div className="signInput w-full">
+                <div className="pl-40 py-6 pr-72">
+                  <p className="text-slate-400 text-l">
+                    Questions? Call 000-800-919-1694
+                  </p>
+                  <div className="flex flex-wrap py-8">
+                    <p className="text-slate-400 text-sm pr-52 pb-3">FAQ</p>
+                    <p className="text-slate-400 text-sm pr-48">HelpCentre</p>
+                    <p className="text-slate-400 text-sm pr-48">Terms of Use</p>
+                    <p className="text-slate-400 text-sm">Privacy</p>
+                    <p className="text-slate-400 text-sm pr-28">
+                      Cookie Preferences
+                    </p>
+                    <p className="text-slate-400 text-sm">
+                      Corporate Information
+                    </p>
+                  </div>
+                  <select className="mb-10 py-2 px-5 bg-black text-white border-[1px] border-gray-300">
+                    <option value="English">English</option>
+                    <option value="Hindi">Hindi</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/authentication/SignUp.js
+++ src/components/authentication/SignUp.js
@@ -10,27 +10,30 @@ const SignUp = () => {
           alt="bg-img"
           className="w-full h-full object-cover overflow-clip"
         />
-        <div className="absolute top-0 right-0 bottom-0 left-0 signBg">
-          <Header />
-          <div className=" text-center w-full mt-72">
-            <h1 className="text-white text-5xl font-bold m-1 py-1">
-              Unlimited movies, TV shows and more
-            </h1>
-            <p className="text-white text-3xl font-semibold m-1 py-4">
-              Watch anywhere. Cancle anytime.
-            </p>
-            <p className="text-white text-2xl font-semibold m-1">
-              Ready to watch? Enter your email to create or restart your
-              membership.
-            </p>
-            <div className="m-1 py-3 flex justify-center">
-              <input
-                placeholder="Email address"
-                className=" text-white signInput bg-transparent text-l px-4 py-4 mx-2 rounded-md w-2/6 border-[1px] border-white"
-              />
-              <button className="bg-red-600 text-2xl font-bold text-white px-8 py-3 rounded-md">
-                Get Started {">"}
-              </button>
+        <div className="absolute top-0 right-0 bottom-0 left-0 signBg1">
+          <div className="absolute top-0 right-0 bottom-0 left-0 signBg2">
+            <Header />
+            <div className=" text-center w-full mt-72">
+              <h1 className="text-white text-5xl font-bold m-1 py-1">
+                Unlimited movies, TV shows and more
+              </h1>
+              <p className="text-white text-3xl font-semibold m-1 py-4">
+                Watch anywhere. Cancle anytime.
+              </p>
+              <p className="text-white text-2xl font-semibold m-1">
+                Ready to watch? Enter your email to create or restart your
+                membership.
+              </p>
+              <div className="m-1 py-3 flex justify-center">
+                <input
+                  placeholder="Email address"
+                  type="email"
+                  className=" text-white signInput bg-transparent text-l px-4 py-4 mx-2 rounded-md w-2/6 border-[1px] border-white"
+                />
+                <button className="bg-red-600 text-2xl font-bold text-white px-8 py-3 rounded-md">
+                  Get Started {">"}
+                </button>
+              </div>
             </div>
           </div>
         </div>
