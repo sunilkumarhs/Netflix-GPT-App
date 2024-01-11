4644324a98487d615c27d8d19dda5f85f7db43b3
sunilkumarhs <sunilkumarhs974117@gmail.com>
2024-01-11 23:42:46 +0530

    "fixed display bug"

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -8,13 +8,15 @@ const MyList = () => {
   return (
     <div className="bg-slate-800 w-screen h-screen">
       <BrowseHeader />
-      <div className=" px-4 py-[6%]">
+      <div className=" px-4 xl:py-[6%] py-[15%]">
         <p className="text-4xl text-white">My List</p>
       </div>
-      <div className="lg:px-1 xl:mx-16 mx-6 flex flex-wrap bg-slate-800">
-        {list?.map((item, index) => (
-          <MovieCards key={index} movieDetails={item} />
-        ))}
+      <div className=" bg-slate-800 w-screen">
+        <div className="lg:px-1 xl:mx-16 mx-6 flex flex-wrap">
+          {list?.map((item, index) => (
+            <MovieCards key={index} movieDetails={item} />
+          ))}
+        </div>
       </div>
     </div>
   );
